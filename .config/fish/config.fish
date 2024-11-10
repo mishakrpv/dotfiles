4eabe79f159919f10956b4222741ce2d89156ead
mishakrpv <mishavkrpv@gmail.com>
2024-11-10 16:29:08 +0300

    feat(*): update dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,8 @@
 set fish_greeting ""
 
-# oh-my-posh
-oh-my-posh init fish --config $HOME/.poshthemes/modified-catppuccin_mocha.omp.json | source
-
-# theme
-set -g theme_color_scheme terminal-dark
-set -g fish_prompt_pwd_dir_length 1
-set -g theme_display_user yes
-set -g theme_hide_hostname no
-set -g theme_hostname always
+zoxide init fish | source
 
 # Go
 set -g GOPATH /usr/local/go
 set -gx PATH $GOPATH/bin $PATH
+set -gx PATH $HOME/go/bin $PATH
