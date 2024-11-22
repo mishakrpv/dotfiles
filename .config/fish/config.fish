706ef9ff1be2f54fc4ad48f849428cb573fe5590
mishakrpv <mishavkrpv@gmail.com>
2024-11-22 05:44:34 +0300

    feat(*): add gnu stow

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-set fish_greeting ""
-
-zoxide init fish | source
-
-# Go
-set -g GOPATH /usr/local/go
-set -gx PATH $GOPATH/bin $PATH
-set -gx PATH $HOME/go/bin $PATH
---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,24 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+zoxide init fish | source
+
+# Go
+set -g GOPATH /usr/local/go
+set -gx PATH $GOPATH/bin $PATH
+set -gx PATH $HOME/go/bin $PATH
+
+switch (uname)
+    case Darwin
+        source (dirname (status --current-filename))/config-osx.fish
+    case Linux
+        source (dirname (status --current-filename))/config-linux.fish
+end
